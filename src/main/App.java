0805da0fca4f7018133b3ee191a4eebd981f9656
Cassiano Castro <development.castro@gmail.com>
2024-02-27 15:14:12 -0300

    Removed try/catch block

--- src/main/App.java
+++ src/main/App.java
@@ -1,16 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package model;
+package main;
 
 import controller.EmployeesController;
-import java.sql.Connection;
-import java.sql.SQLException;
-import model.dao.ConnectionFactory;
+import view.EmployeeIndexView;
 
 /**
- *
  *
  */
 public class App
@@ -18,12 +11,8 @@ public class App
 
     public void start()
     {
-        try (Connection connection = new ConnectionFactory().getConnection())
-        {
-            new EmployeesController(connection).caller();
-        } catch (SQLException e)
-        {
-            System.out.println(e.getMessage());
-        }
+        var controller = new EmployeesController();
+
+        new EmployeeIndexView(controller);
     }
 }
