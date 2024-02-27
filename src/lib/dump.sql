d7e996815b2091d612f8e20b0ef9f691001ad041
Cassiano Castro <development.castro@gmail.com>
2024-02-27 15:06:59 -0300

    Removed unnecessary comments

--- src/lib/dump.sql
+++ src/lib/dump.sql
@@ -1,13 +1,3 @@
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Other/SQLTemplate.sql to edit this template
- */
-
-/**
- * 
- * Created: 18 de mai de 2022
- */
-
 CREATE TABLE IF NOT EXISTS employee (
     PK_ID     INTEGER PRIMARY KEY,
     name      TEXT NOT NULL,
@@ -16,9 +6,9 @@ CREATE TABLE IF NOT EXISTS employee (
     birthDate INTEGER NOT NULL
 );
 
-INSERT INTO 
-    employee(name, sex, cpf, birthDate) 
+INSERT INTO
+    employee(name, sex, cpf, birthDate)
 VALUES
-    ('Ana Maria', 'f', '123.456.789/00', 984452400000),
-    ('Bia Mattos', 'f', '456.789.123/00', 740458800000),
-    ('Carlos Lopes', 'm', '789.456.123/00', 951012000000);
+    ('Ana Maria', 'F', '123.456.789/00', 984452400000),
+    ('Bia Mattos', 'F', '456.789.123/00', 740458800000),
+    ('Carlos Lopes', 'M', '789.456.123/00', 951012000000);
